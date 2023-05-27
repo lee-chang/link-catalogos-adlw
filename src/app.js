8aa784d70289cd23b28c24d1bd3bbb5649ac9709
vchangcar <ca.y25@hotmail.com>
2023-05-27 09:45:38 -0500

    update

--- src/app.js
+++ src/app.js
@@ -1,111 +1,111 @@
-const {chromium} = require( 'playwright');
-var CronJob = require('cron').CronJob;
+  const {chromium} = require( 'playwright');
+  var CronJob = require('cron').CronJob;
 
-const express = require('express');
-const serveIndex = require('serve-index');
-const bodyParser = require('body-parser');
-const path = require('path');
-const fs = require('fs');
+  const express = require('express');
+  const serveIndex = require('serve-index');
+  const bodyParser = require('body-parser');
+  const path = require('path');
+  const fs = require('fs');
 
-var app = express();
-app.use(express.static('public'));
+  var app = express();
+  app.use(express.static('public'));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.resolve('src/index.html'));
-});
+  app.get('/', function(req, res) {
+    res.sendFile(path.resolve('src/index.html'));
+  });
 
-//express static files
-app.use('/public', express.static('src/public'));
+  //express static files
+  app.use('/public', express.static('src/public'));
 
-app.use('/catalogos', express.static('src/catalogos/upload'), serveIndex('src/catalogos/upload', {
-  icons: true,
-  template: 'src/catalogos/template.html',
-}))
-
-
-
-var job = new CronJob(
-	'*/5 * * * *',
-	function() {
-        Main.update();
-	},
-	null,
-	true,
-	'America/Los_Angeles'
-);
+  app.use('/catalogos', express.static('src/catalogos/upload'), serveIndex('src/catalogos/upload', {
+    icons: true,
+    template: 'src/catalogos/template.html',
+  }))
 
 
-const PORT = process.env.PORT || 3000;
-// Listen
-app.listen(PORT,function(){
-    console.log('Servidor listo en el puerto',PORT);
-})
 
-class Main {
-  static async update(){
+  var job = new CronJob(
+    '*/5 * * * *',
+    function() {
+          Main.update();
+    },
+    null,
+    true,
+    'America/Los_Angeles'
+  );
 
-      (async () => {
 
-          const browser = await chromium.launch()
-          const page = await browser.newPage()
+  const PORT = process.env.PORT || 3000;
+  // Listen
+  app.listen(PORT,function(){
+      console.log('Servidor listo en el puerto',PORT);
+  })
 
-          //Preparando la pagina
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual',  {timeout: 0} )
-      
-          //Todos los productos talla 35
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=3',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Talla35-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
-      
-          //Todos los productos talla 36
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=4',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Talla36-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
-      
-          //Todos los productos talla 37
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=5',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Talla37-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
-      
-          //Todos los productos talla 38
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=6',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Talla38-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
-      
-          //Todos los productos talla 39
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=7',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Talla39-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
+  class Main {
+    static async update(){
 
-          //Todos los productos Botines
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idCategoria=84',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Botines-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
+        (async () => {
 
-          //Todos los productos Botas
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idCategoria=87',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Botas-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
+            const browser = await chromium.launch()
+            const page = await browser.newPage()
 
-          //Todos los productos Cerrados
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idCategoria=83',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Cerrados-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
+            //Preparando la pagina
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual',  {timeout: 0} )
+        
+            //Todos los productos talla 35
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=3',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Talla35-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},  printBackground: true, preferCSSPageSize:true , preferCSSPageSize:true })
+        
+            //Todos los productos talla 36
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=4',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Talla36-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'}, printBackground: true, preferCSSPageSize:true })
+        
+            //Todos los productos talla 37
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=5',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Talla37-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'}, printBackground: true, preferCSSPageSize:true })
+        
+            //Todos los productos talla 38
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=6',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Talla38-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'}, printBackground: true, preferCSSPageSize:true })
+        
+            //Todos los productos talla 39
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idTalla=7',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Talla39-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'}, printBackground: true, preferCSSPageSize:true })
 
-          //Todos los productos Sandalias
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idCategoria=88',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Sandalias-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
+            //Todos los productos Botines
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idCategoria=84',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Botines-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'}, printBackground: true, preferCSSPageSize:true })
 
-          //Todos los productos
-          await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual',  {timeout: 0} )
-          await page.waitForTimeout(5000);
-          await page.pdf({ path: 'src/catalogos/upload/Todas-Tallas-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'},printBackground: true})
-      
-          await browser. close()
-      
-          console.log('PDFs generados correctamente')
-      
-      })()
+            //Todos los productos Botas
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idCategoria=87',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Botas-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'}, printBackground: true, preferCSSPageSize:true })
+
+            //Todos los productos Cerrados
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idCategoria=83',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Cerrados-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'}, printBackground: true, preferCSSPageSize:true })
+
+            //Todos los productos Sandalias
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual?idCategoria=88',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Sandalias-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'}, printBackground: true, preferCSSPageSize:true })
+
+            //Todos los productos
+            await page.goto('https://zicca.pe/Productos/ListaProductosTiendaVirtual',  {timeout: 0} )
+            await page.waitForTimeout(5000);
+            await page.pdf({ path: 'src/catalogos/upload/Todas-Tallas-Zicca.pdf', width: '21cm', height: '29.6cm', margin: {top: '1cm', bottom: '1cm', left: '1cm', right: '1cm'}, printBackground: true, preferCSSPageSize:true })
+        
+            await browser. close()
+        
+            console.log('PDFs generados correctamente')
+        
+        })()
+    }
   }
-}
